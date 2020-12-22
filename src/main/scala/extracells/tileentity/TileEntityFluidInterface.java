1ae960bf70faf51e033a6a048a5e7cca90516399
Lex <domian844@gmail.com>
2020-12-22 10:06:23 +0700

    ???

--- src/main/scala/extracells/tileentity/TileEntityFluidInterface.java
+++ src/main/scala/extracells/tileentity/TileEntityFluidInterface.java
@@ -931,7 +931,6 @@ public class TileEntityFluidInterface extends TileBase implements
 
 	private NBTTagCompound writeOutputToNBT(NBTTagCompound tag) {
 		int i = 0;
-		i = 0;
 		for (IAEStack s : this.addToExport) {
 			if (s != null) {
 				tag.setBoolean("add-" + i + "-isItem", s.isItem());
@@ -940,7 +939,7 @@ public class TileEntityFluidInterface extends TileBase implements
 					((IAEItemStack) s).getItemStack().writeToNBT(data);
 				} else {
 					((IAEFluidStack) s).getFluidStack().writeToNBT(data);
-				};
+				}
 				tag.setTag("add-" + i, data);
 				tag.setLong("add-" + i + "-amount", s.getStackSize());
 			}
